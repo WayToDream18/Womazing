522f878f4c8f63924761b43b4626b446c5b74f66
WayToDream18 <114391247+WayToDream18@users.noreply.github.com>
2022-09-27 12:12:24 +0300

    Womazing
    Womazing

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$('.dream__slider').slick({
+	arrows: true,
+	prevArrow: '<button class="slick-prev"><img src="img/arrow-left.svg"></button>',
+	nextArrow: '<button class="slick-next"><img src="img/arrow-right.svg"></button>'
+});
